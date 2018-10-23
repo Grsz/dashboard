1c7eb4fa64b78d6004f5789b977f17a62ccbe17c
Grsz <39468871+Grsz@users.noreply.github.com>
2018-10-23 22:37:05 +0100

    Delete db.js

--- server/db.js
+++
@@ -1,9 +0,0 @@
-exports.db = require('knex')({
-    client: 'pg',
-    connection: {
-      host : '127.0.0.1',
-      user : 'hrger',
-      password : 'Xezeru007',
-      database : 'therapy'
-    }
-  });
